ac170e49fb387f3766d3743f0db80d90a0566101
Diego Carvajal <diego98cs@gmail.com>
2025-02-09 20:38:41 -0500

    Added ESP32 serial communication program and ros2_control template

---
+++ xarm_control/hardware/xarm_hardware.cpp
@@ -0,0 +1,75 @@
+// Copyright 2023 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xarm_control/xarm_hardware.hpp"
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+namespace xarm_control
+{
+CallbackReturn XArmSystemHardware::on_init(const hardware_interface::HardwareInfo & info)
+{
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+  {
+    return CallbackReturn::ERROR;
+  }
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+CallbackReturn XArmSystemHardware::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // reset values always when configuring hardware
+  for (const auto & [name, descr] : joint_state_interfaces_)
+  {
+    set_state(name, 0.0);
+  }
+  for (const auto & [name, descr] : joint_command_interfaces_)
+  {
+    set_command(name, 0.0);
+  }
+  for (const auto & [name, descr] : sensor_state_interfaces_)
+  {
+    set_state(name, 0.0);
+  }
+
+  return CallbackReturn::SUCCESS;
+}
+
+return_type XArmSystemHardware::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
+{
+  // TODO(pac48) set sensor_states_ values from subscriber
+
+  for (std::size_t i = 0; i < info_.joints.size(); i++)
+  {
+    const auto name_vel = info_.joints[i].name + "/" + hardware_interface::HW_IF_VELOCITY;
+    const auto name_pos = info_.joints[i].name + "/" + hardware_interface::HW_IF_POSITION;
+    set_state(name_vel, get_command(name_vel));
+    set_state(name_pos, get_state(name_pos) + get_state(name_vel) * period.seconds());
+  }
+  return return_type::OK;
+}
+
+return_type XArmSystemHardware::write(const rclcpp::Time &, const rclcpp::Duration &)
+{
+  return return_type::OK;
+}
+
+}  // namespace xarm_control
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  xarm_control::XArmSystemHardware, hardware_interface::SystemInterface)
---
+++ xarm_control/xArm_Serial/src/main.cpp
@@ -0,0 +1,254 @@
+#include <Arduino.h>
+#include <lx16a-servo.h>
+
+LX16ABus servoBus;
+LX16AServo servo1(&servoBus, 1); // 0-16800 (0-168 degrees)
+LX16AServo servo2(&servoBus, 2); // 0-24000 (0-240 degrees)
+LX16AServo servo3(&servoBus, 3); // 0-24000 (0-240 degrees)
+LX16AServo servo4(&servoBus, 4); // 0-24000 (0-240 degrees)
+LX16AServo servo5(&servoBus, 5); // 0-24000 (0-240 degrees)
+LX16AServo servo6(&servoBus, 6); // 0-24000 (0-240 degrees)
+
+#define READ_ENCODERS 'e'
+#define MOTOR_SPEEDS 'm'
+
+/* Variable initialization */
+
+// A pair of varibles to help parse serial commands (thanks Fergs)
+int arg = 0;
+int arg_index = 0;
+
+// Variable to hold an input character
+char chr;
+
+// Variable to hold the current single-character command
+char cmd;
+
+// Character arrays to hold the first and second arguments
+char argv1[16];
+char argv2[16];
+char argv3[16];
+char argv4[16];
+char argv5[16];
+char argv6[16];
+
+// The arguments converted to integers
+long argi[6];
+
+int pos_cmd[6]; // Servo pos commands
+
+double pos[6] = {0, 0, 0, 0, 0, 0}; // servo positions
+
+void runCommand()
+{
+  argi[0] = atoi(argv1);
+  argi[1] = atoi(argv2);
+  argi[2] = atoi(argv3);
+  argi[3] = atoi(argv4);
+  argi[4] = atoi(argv5);
+  argi[5] = atoi(argv6);
+
+  switch (cmd)
+  {
+  case READ_ENCODERS:
+    pos[0] = servo1.pos_read();
+    pos[1] = servo2.pos_read();
+    pos[2] = servo3.pos_read();
+    pos[3] = servo4.pos_read();
+    pos[4] = servo5.pos_read();
+    pos[5] = servo6.pos_read();
+
+    Serial.print(pos[0]);
+    Serial.print(" ");
+    Serial.print(pos[1]);
+    Serial.print(" ");
+    Serial.print(pos[2]);
+    Serial.print(" ");
+    Serial.print(pos[3]);
+    Serial.print(" ");
+    Serial.print(pos[4]);
+    Serial.print(" ");
+    Serial.print(pos[5]);
+    Serial.println("\r");
+    break;
+  case MOTOR_SPEEDS:
+    for(int i = 0; i < 6; i++)
+    {
+      pos_cmd[i] = argi[i] * 10;
+    }
+
+    servo1.move_time(pos_cmd[0], 1000);
+    servo2.move_time(pos_cmd[1], 1000);
+    servo3.move_time(pos_cmd[2], 1000);
+    servo4.move_time(pos_cmd[3], 1000);
+    servo5.move_time(pos_cmd[4], 1000);
+    servo6.move_time(pos_cmd[5], 1000);
+
+    Serial.println("OK\r");
+    break;
+  default:
+    Serial.println("Invalid Command\r");
+    break;
+  }
+}
+
+/* Clear the current command parameters */
+void resetCommand()
+{
+  cmd = NULL;
+  memset(argv1, 0, sizeof(argv1));
+  memset(argv2, 0, sizeof(argv2));
+  memset(argv3, 0, sizeof(argv3));
+  memset(argv4, 0, sizeof(argv4));
+  memset(argv5, 0, sizeof(argv5));
+  memset(argv6, 0, sizeof(argv6));
+
+  memset(argi, 0, sizeof(argi));
+  arg = 0;
+  arg_index = 0;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.println("Beginning Servo Example");
+  servoBus.beginOnePinMode(&Serial2, 33);
+  servoBus.debug(true);
+  servoBus.retry = 3;
+
+  // Reset the servo positions
+  servo1.move_time(16000, 1000); // Home: 16000
+  servo2.move_time(11000, 1250); // Home: 11000
+  servo3.move_time(12000, 1500); // Home: 12000
+  servo4.move_time(11750, 2000); // Home: 11750
+  servo5.move_time(12250, 2000); // Home: 12250
+  servo6.move_time(11750, 2000); // Home: 11750
+  delay(2000);
+
+  servo1.move_time(8000, 500);
+  delay(500);
+  servo1.move_time(16000, 500);
+  delay(500);
+  servo1.move_time(7000, 500);
+  delay(500);
+
+  // Read in servo positions
+  pos[0] = servo1.pos_read();
+  delay(1000);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    while (Serial.available() > 0)
+    {
+
+      // Read the next character
+      chr = Serial.read();
+
+      // Terminate a command with a CR
+      if (chr == 13)
+      {
+        if (arg == 6)
+          argv6[arg_index] = NULL;
+
+        runCommand();
+        resetCommand();
+      }
+      // Use spaces to delimit parts of the command
+      else if (chr == ' ')
+      {
+        // Step through the arguments
+        switch (arg)
+        {
+        case 0:
+          arg = 1;
+          break;
+
+        case 1:
+          argv1[arg_index] = NULL;
+          arg++;
+          arg_index = 0;
+          break;
+
+        case 2:
+          argv2[arg_index] = NULL;
+          arg++;
+          arg_index = 0;
+          break;
+
+        case 3:
+          argv3[arg_index] = NULL;
+          arg++;
+          arg_index = 0;
+          break;
+
+        case 4:
+          argv4[arg_index] = NULL;
+          arg++;
+          arg_index = 0;
+          break;
+
+        case 5:
+          argv5[arg_index] = NULL;
+          arg++;
+          arg_index = 0;
+          break;
+
+        default:
+          break;
+        }
+        continue;
+      }
+      else
+      {
+        switch (arg)
+        {
+        case 0:
+          // First argument is always a single letter command
+          cmd = chr;
+          break;
+
+        case 1:
+          // Subsequent arguments can be more than one character
+          argv1[arg_index] = chr;
+          arg_index++;
+          break;
+
+        case 2:
+          argv2[arg_index] = chr;
+          arg_index++;
+          break;
+
+        case 3:
+          argv3[arg_index] = chr;
+          arg_index++;
+          break;
+
+        case 4:
+          argv4[arg_index] = chr;
+          arg_index++;
+          break;
+
+        case 5:
+          argv5[arg_index] = chr;
+          arg_index++;
+          break;
+
+        case 6:
+          argv6[arg_index] = chr;
+          arg_index++;
+          break;
+
+        default:
+          break;
+        }
+      }
+    }
+  }
+}
